d84ee82f488d2a3cf86371cec1112703cc15e955
djmakwana797 <dmakwana979@rku.ac.in>
2020-10-19 18:21:54 +0530

    Initial commit

---
+++ app/src/main/java/com/rku/tutorial13/MainActivity.java
@@ -0,0 +1,21 @@
+package com.rku.tutorial13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void callToNumber(View view) {
+    }
+
+    public void sendTextMessage(View view) {
+    }
+}
